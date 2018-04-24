7510a0c9347003afa070ec814201db04bfa9d82a
Camnh <hongcam941997@gmail.com>
2018-04-24 10:13:18 +0700

    movies_view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def Index index
+		  index = index.zero? ? index = 1 : index += 1
+	end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w(admin.css)
-Rails.application.config.assets.precompile += %w(admin.js)
+Rails.application.config.assets.precompile += %w(admin.js index_admin.js)
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
